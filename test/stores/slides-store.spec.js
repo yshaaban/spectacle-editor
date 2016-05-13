2ecbad2a162bc69e84590eed2f77966e80fdbff0
Rob Gerstenberger <rob.gerstenberger@formidable.com>
2016-05-13 15:29:21 -0700

    add slides-store test scaffolding

--- test/stores/slides-store.spec.js
+++ test/stores/slides-store.spec.js
@@ -5,14 +5,79 @@ import { autorun } from "mobx";
 import SlidesStore from "../../app/stores/slides-store";
 
 let slideStore;
+let boxOfSand;
+let historySpy;
+let historyIndexSpy;
+let currentSlideSpy;
+let currentSlideIndexSpy;
+let slidesSpy;
+let currentElementSpy;
+let currentElementIndexSpy;
+let undoDisabledSpy;
+let redoDisabledSpy;
+
+const disposers = [];
 
 describe("SlidesStore", () => {
   beforeEach(() => {
+    boxOfSand = sandbox.create();
     slideStore = new SlidesStore();
+
+    historySpy = boxOfSand.spy();
+    historyIndexSpy = boxOfSand.spy();
+    currentSlideSpy = boxOfSand.spy();
+    currentSlideIndexSpy = boxOfSand.spy();
+    slidesSpy = boxOfSand.spy();
+    currentElementSpy = boxOfSand.spy();
+    currentElementIndexSpy = boxOfSand.spy();
+    undoDisabledSpy = boxOfSand.spy();
+    redoDisabledSpy = boxOfSand.spy();
+
+    disposers.push(autorun(() => {
+      historySpy(slideStore.history);
+    }));
+
+    disposers.push(autorun(() => {
+      historyIndexSpy(slideStore.historyIndex);
+    }));
+
+    disposers.push(autorun(() => {
+      currentSlideSpy(slideStore.currentSlide);
+    }));
+
+    disposers.push(autorun(() => {
+      currentSlideIndexSpy(slideStore.currentSlideIndex);
+    }));
+
+    disposers.push(autorun(() => {
+      slidesSpy(slideStore.slides);
+    }));
+
+    disposers.push(autorun(() => {
+      currentElementSpy(slideStore.currentElement);
+    }));
+
+    disposers.push(autorun(() => {
+      currentElementIndexSpy(slideStore.currentElementIndex);
+    }));
+
+    disposers.push(autorun(() => {
+      undoDisabledSpy(slideStore.undoDisabled);
+    }));
+
+    disposers.push(autorun(() => {
+      redoDisabledSpy(slideStore.redoDisabled);
+    }));
   });
 
   afterEach(() => {
+    slideStore = null;
 
+    disposers.forEach((dispose) => {
+      dispose();
+    });
+
+    boxOfSand.restore();
   });
 
   it("should initialize with correct data", () => {
@@ -29,5 +94,15 @@ describe("SlidesStore", () => {
 
     expect(slides.length).to.equal(5);
     expect(slides[0]).to.eql(currentSlide);
+
+    expect(historySpy).to.not.have.been.called;
+    expect(historyIndexSpy).to.not.have.been.called;
+    expect(currentSlideSpy).to.not.have.been.called;
+    expect(currentSlideIndexSpy).to.not.have.been.called;
+    expect(slidesSpy).to.not.have.been.called;
+    expect(currentElementSpy).to.not.have.been.called;
+    expect(currentElementIndexSpy).to.not.have.been.called;
+    expect(undoDisabledSpy).to.not.have.been.called;
+    expect(redoDisabledSpy).to.not.have.been.called;
   });
 });
