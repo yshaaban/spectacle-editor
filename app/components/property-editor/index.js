8f04331cbd6d0f065d05225e22f0ba0d7206a872
Mike Axtman <mike.axtman@formidable.com>
2016-06-10 17:49:24 -0700

    Revert "update styles for contextual menus and add transitional animations between menus upon selection"
    This reverts commit 40e0482f1859d40e36b5f6b1ae2115ecda84547e.

--- app/components/property-editor/index.js
+++ app/components/property-editor/index.js
@@ -1,40 +1,13 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
-import { autorun } from "mobx";
 
 import styles from "./index.css";
-import SlideMenu from "./contextual-menus/slides";
-import TextMenu from "./contextual-menus/text";
-import ImageMenu from "./contextual-menus/image";
-import { ElementTypes } from "../../constants";
 
 @observer
 class PropertyEditor extends Component {
   static contextTypes = {
     store: React.PropTypes.object
   };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasMenu: false,
-      contextualMenu: null
-    };
-  }
-
-  componentDidMount() {
-    autorun(() => {
-      if (this.context.store.currentElement) {
-        this.setState({
-          hasMenu: true,
-          contextualMenu: this.context.store.currentElement.type
-        });
-        return;
-      }
-
-      this.setState({ hasMenu: false, contextualMenu: null });
-    });
-  }
 
   onPropertyChange = (e, key) => {
     let value = e.target.value;
@@ -53,26 +26,49 @@ class PropertyEditor extends Component {
   }
 
   render() {
-    const moveMenu = this.state.hasMenu ? styles.slidesInactive : "";
-    const { contextualMenu } = this.state;
+    const { currentElement } = this.context.store;
+    let content;
+
+    if (currentElement) {
+      const props = currentElement.props;
+      const propertyKeys = Object.keys(props);
+      const style = props.style;
+      const styleKeys = Object.keys(style);
+
+      // TODO: which properties should actually be exposed to the user, and where is that defined
+      // TODO: where should the ui elements for each type be defined
+      content = (<div>
+        <h2>{currentElement.type}</h2>
+        <ul>
+          {propertyKeys.map((key) => {
+            if (key === "style") return false;
+            return (<li key={`${props.id}-${key}`}>
+              <h4>{key}</h4>
+              <input
+                type="number"
+                value={props[key]}
+                onChange={(e) => this.onPropertyChange(e, key)}
+              />
+            </li>);
+          })}
+          {styleKeys.map((key) => {
+            if (key === "style") return false;
+            return (<li key={`${props.id}-${key}`}>
+              <h4>{key}</h4>
+              <input
+                type="text"
+                value={style[key]}
+                onChange={(e) => this.onStyleChange(e, key)}
+              />
+            </li>);
+          })}
+        </ul>
+      </div>);
+    }
 
     return (
       <div className={styles.editor}>
-        <div className={`${styles.menu} ${moveMenu}`}>
-          <SlideMenu />
-        </div>
-        <div className={`${styles.menu} ${styles.contextMenu}
-           ${(ElementTypes.TEXT === contextualMenu) ?
-          styles.contextMenuActive : ""}`}
-        >
-          <TextMenu />
-        </div>
-        <div className={`${styles.menu} ${styles.contextMenu}
-           ${(ElementTypes.IMAGE === contextualMenu) ?
-          styles.contextMenuActive : ""}`}
-        >
-          <ImageMenu />
-        </div>
+        {content}
       </div>
     );
   }
