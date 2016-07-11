a8506a304beebf4c249da40ba35f8ade4ba7e199
Mike Axtman <mike.axtman@formidable.com>
2016-07-11 11:04:45 -0700

    attempting to add lists through css

--- app/components/property-editor/editor-components/list.js
+++ app/components/property-editor/editor-components/list.js
@@ -2,15 +2,46 @@ import React, { Component } from "react";
 import styles from "./list.css";
 
 export default class List extends Component {
+  static propTypes = {
+    currentElement: React.PropTypes.object
+  }
+
+  static contextTypes = {
+    store: React.PropTypes.object
+  }
+
+  createClickHandler = (nextListType) => (ev) => {
+    ev.preventDefault();
+    const currentListType = this.props.currentElement.props.listType;
+
+    this.context.store.updateElementProps({
+      listType: currentListType === nextListType ? null : nextListType
+    });
+  }
+
   render() {
+    const { currentElement } = this.props;
+
     return (
       <div className={styles.listWrapper}>
         <span
-          className={`octicon octicon-list-unordered ${styles.listBox}`}
+          onClick={this.createClickHandler("unordered")}
+          className={
+            `octicon
+             octicon-list-unordered
+             ${styles.listBox}
+             ${currentElement.props.listType === "unordered" ? styles.selectedList : ""}`
+          }
         >
         </span>
         <span
-          className={`octicon octicon-list-ordered ${styles.listBox}`}
+          onClick={this.createClickHandler("ordered")}
+          className={
+            `octicon
+             octicon-list-ordered
+             ${styles.listBox}
+             ${currentElement.props.listType === "ordered" ? styles.selectedList : ""}`
+          }
         >
         </span>
       </div>
