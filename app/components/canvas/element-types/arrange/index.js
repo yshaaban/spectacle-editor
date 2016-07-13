191dd311b3f07dc5a177b1d9a4534c0b1a307bfb
Paula Lavalle <lavalle.paula@gmail.com>
2016-07-13 16:04:43 -0700

    lint; add click handlers without functionality

--- app/components/canvas/element-types/arrange/index.js
+++ app/components/canvas/element-types/arrange/index.js
@@ -1,47 +1,55 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
 import { BRINGTOFRONT, BRINGFORWARD, SENDBACKWARD, SENDTOBACK } from "../../../../assets/icons";
 
 import styles from "./index.css";
 
 class Arrange extends Component {
-  static contextTypes = {
-    store: PropTypes.object
-  };
-
-  onClickAddSlide = () => {
-    this.context.store.addSlide();
+  onClickFront = () => {
+    // TODO
+    console.log("Bring to Front!");
   }
 
-  onClickDeleteSlide = () => {
-    this.context.store.deleteSlide();
+  onClickForward = () => {
+    // TODO
+    console.log("Increment forward by 1");
+  }
+
+  onClickBackward = () => {
+    // TODO
+    console.log("Decrement backward by 1");
+  }
+
+  onClickBack = () => {
+    // TODO
+    console.log("Send to back!");
   }
 
   render() {
     return (
       <div className={styles.arrangeContainer}>
         <div className={styles.arrange}>
-          <button className={styles.arrangeButton}>
+          <button className={styles.arrangeButton} onClick={this.onClickFront}>
             <i
               className={styles.arrangeIcon}
               dangerouslySetInnerHTML={{ __html: BRINGTOFRONT }}
               title="Bring to front"
             />
           </button>
-          <button className={styles.arrangeButton}>
+          <button className={styles.arrangeButton} onClick={this.onClickForward}>
             <i
               className={styles.arrangeIcon}
               dangerouslySetInnerHTML={{ __html: BRINGFORWARD }}
               title="Bring forward"
             />
           </button>
-          <button className={styles.arrangeButton}>
+          <button className={styles.arrangeButton} onClick={this.onClickBackward}>
             <i
               className={styles.arrangeIcon}
               dangerouslySetInnerHTML={{ __html: SENDBACKWARD }}
               title="Send backward"
             />
           </button>
-          <button className={styles.arrangeButton}>
+          <button className={styles.arrangeButton} onClick={this.onClickBack}>
             <i
               className={styles.arrangeIcon}
               dangerouslySetInnerHTML={{ __html: SENDTOBACK }}
