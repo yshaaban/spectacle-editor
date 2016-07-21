ffe0824af21a0801bacdb7b645affac46fdd20fd
Paula Lavalle <lavalle.paula@gmail.com>
2016-07-21 14:32:43 -0700

    Add global input styles; WIP link functionality

--- app/components/property-editor/editor-components/linkto.js
+++ app/components/property-editor/editor-components/linkto.js
@@ -2,13 +2,40 @@ import React, { Component } from "react";
 import styles from "./linkto.css";
 
 export default class LinkTo extends Component {
+  static contextTypes = {
+    store: React.PropTypes.object
+  }
+
+  static propTypes = {
+    currentElement: React.PropTypes.object
+  }
+
+  handleChange = (ev) => {
+    console.log(ev);
+    const value = ev.target.value;
+    console.log("value", value);
+  }
+
+
+  handleCursorPosition(ev) {
+    const { value } = ev.target;
+    const numLength = value.length;
+
+    ev.target.setSelectionRange(0, numLength);
+  }
+
   render() {
     return (
-      <input
-        className={styles.inputBox}
-        placeholder="http://"
-        type="text"
-      />
+      <div>
+        <i className={`ion-link`} />
+        <input
+          className={`globalInput ${styles.input}`}
+          placeholder="http://"
+          type="text"
+          onChange={this.handleChange}
+          onFocus={this.handleCursorPosition}
+        />
+      </div>
     );
   }
 }
